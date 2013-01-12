3a97fc80966519863076939ef93d138718934760
so_c <so_c321@hotmail.co.jp>
2013-01-13 01:17:50 +0900

    add test for divide.

--- junit-tutorial/test/junit/tutorial/CalculatorTest.java
+++ junit-tutorial/test/junit/tutorial/CalculatorTest.java
@@ -14,4 +14,12 @@ public class CalculatorTest {
 		assertThat(actual, is(expected));
 	}
 
+	@Test
+	public void testDevideCanGetResultUsingIntValues() {
+		Calculator calculator = new Calculator();
+		float expected = 1.5f;
+		float actual = calculator.divide(3, 2);
+		assertThat(actual, is(expected));
+	}
+	
 }
