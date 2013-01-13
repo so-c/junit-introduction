c7264509cf270c8579fdffdfb5d19ab4dc5748fc
so_c <so_c321@hotmail.co.jp>
2013-01-13 14:04:22 +0900

    Add temporal implements and its test.

---
+++ junit-practice/src/chapter18/practice1/StringUtils.java
@@ -0,0 +1,11 @@
+package chapter18.practice1;
+
+public class StringUtils {
+	private StringUtils() {
+		super();
+	}
+	
+	public static String toSnakeCase(String string) {
+		return string;
+	}
+}
---
+++ junit-practice/test/chapter18/practice1/StringUtilsTest.java
@@ -0,0 +1,29 @@
+package chapter18.practice1;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilsTest {
+	@Test
+	public void testToSnakeCase_return_aaa_when_input_aaa() {
+		String expected = "aaa";
+		String actual = StringUtils.toSnakeCase("aaa");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void testToSnakeCase_return_hello_world_when_input_HelloWorld() {
+		String expected = "hello_world";
+		String actual = StringUtils.toSnakeCase("HelloWorld");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void testToSnakeCase_return_practice_junit_when_input_PracticeJunit() {
+		String expected = "practice_junit";
+		String actual = StringUtils.toSnakeCase("PracticeJunit");
+		assertThat(actual, is(expected));
+	}
+}
