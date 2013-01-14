9da7875c57a4a36dadfa43d2a93b60475665a66a
so_c <so_c321@hotmail.co.jp>
2013-01-14 15:52:12 +0900

    Use enclosed.

--- junit-practice/test/chapter18/practice3/CounterTest.java
+++ junit-practice/test/chapter18/practice3/CounterTest.java
@@ -5,31 +5,52 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class CounterTest {
-	private Counter counter;
+	private static Counter counter;
 
-	@Before
-	public void setUp() {
-		this.counter = new Counter();
+	public static class InitialState {
+		@Before
+		public void setUp() {
+			counter = new Counter();
+		}
+
+		@Test
+		public void testIncrement_return_1_when_first_call() {
+			assertThat(counter.increment(), is(1));
+		}
+
 	}
 
-	@Test
-	public void testIncrement_return_1_when_first_call() {
-		assertThat(counter.increment(), is(1));
-	}
-
-	@Test
-	public void testIncrement_return_1_when_second_call() throws Exception {
-		counter.increment();
-		assertThat(counter.increment(), is(2));
-	}
-
-	@Test
-	public void testIncrement_return_2_when_51th_call() throws Exception {
-		for (int i = 0; i < 50; i++) {
+	public static class OneceIncremented {
+		@Before
+		public void setUp() {
+			counter = new Counter();
 			counter.increment();
 		}
-		assertThat(counter.increment(), is(51));
+
+		@Test
+		public void testIncrement_return_1_when_second_call() throws Exception {
+			assertThat(counter.increment(), is(2));
+		}
 	}
+
+	public static class FiftyTimesIncremented {
+		@Before
+		public void setUp() {
+			counter = new Counter();
+			for (int i = 0; i < 50; i++) {
+				counter.increment();
+			}
+		}
+
+		@Test
+		public void testIncrement_return_2_when_51th_call() throws Exception {
+			assertThat(counter.increment(), is(51));
+		}
+	}
+
 }
