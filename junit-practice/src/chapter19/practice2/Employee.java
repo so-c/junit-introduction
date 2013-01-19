cb8e924e77025a1e4b9a655ce86f09fa2e6a09fb
so_c <so_c321@hotmail.co.jp>
2013-01-19 18:47:03 +0900

    add try-with-resource block.

--- junit-practice/src/chapter19/practice2/Employee.java
+++ junit-practice/src/chapter19/practice2/Employee.java
@@ -11,59 +11,64 @@ public class Employee {
     private String firstName;
     private String lastName;
     private String email;
-    
+
     public Employee() {
         super();
     }
-    
+
     public Employee(String firstName, String lastName, String email) {
         super();
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
-    
+
     public String getFirstName() {
         return firstName;
     }
-    
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    
+
     public String getLastName() {
         return lastName;
     }
-    
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    
+
     public String getEmail() {
         return email;
     }
-    
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
     public static List<Employee> load(InputStream input) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        
         List<Employee> result = new ArrayList<>();
-        String csvRecord;
-        while((csvRecord = reader.readLine())!=null) {
-            result.add(createInstance(csvRecord));
+        
+        try (BufferedReader reader =
+                new BufferedReader(new InputStreamReader(input))) {
+            while (true) {
+                String csvRecord = reader.readLine();
+                if (csvRecord == null) {
+                    break;
+                }
+                result.add(createInstance(csvRecord));
+            }
         }
-                
+
         return result;
     }
-    
+
     private static Employee createInstance(String csvRecord) {
         String[] attributes = csvRecord.split(",");
-        String firstName = attributes[0];
-        String lastName = attributes[1];
-        String email = attributes[2];
+        String firstName = attributes[0].trim();
+        String lastName = attributes[1].trim();
+        String email = attributes[2].trim();
         return new Employee(firstName, lastName, email);
     }
 
@@ -71,8 +76,8 @@ public class Employee {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("Employee [firstName=").append(firstName)
-               .append(", lastName=").append(lastName)
-               .append(", email=").append(email).append("]");
+                .append(", lastName=").append(lastName).append(", email=")
+                .append(email).append("]");
         return builder.toString();
     }
 }
